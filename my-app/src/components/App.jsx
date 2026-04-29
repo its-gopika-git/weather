dc00f616d831f2ffbdde95adf9b5105b921cbe47
gopika <gopika.31g@gmail.com>
2026-04-29 18:59:20 +0530

    deployment-changes

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,12 +1,14 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+
 
 export const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </HashRouter>
-  );
-};
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
